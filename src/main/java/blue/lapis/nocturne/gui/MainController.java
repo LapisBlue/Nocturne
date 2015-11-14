b12fbef175f2da2b0b7679645e9e60947691d6dc
Jamie Mansfield <malgm@lapis.blue>
2015-11-14 18:21:46 +0000

    Start gui controller

---
+++ src/main/java/blue/lapis/nocturne/gui/MainController.java
@@ -0,0 +1,65 @@
+/*
+ * Nocturne
+ * Copyright (c) 2015, Lapis <https://github.com/LapisBlue>
+ *
+ * The MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package blue.lapis.nocturne.gui;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.MenuItem;
+
+/**
+ * The main Java.FX controller.
+ */
+public class MainController {
+
+    public MenuItem openJarButton;
+    public MenuItem closeJarButton;
+    public MenuItem openMappingsButton;
+    public MenuItem closeMappingsButton;
+    public MenuItem saveMappingsButton;
+    public MenuItem closeButton;
+
+    public void openJar(ActionEvent actionEvent) {
+
+    }
+
+    public void closeJar(ActionEvent actionEvent) {
+
+    }
+
+    public void openMappings(ActionEvent actionEvent) {
+
+    }
+
+    public void closeMappings(ActionEvent actionEvent) {
+
+    }
+
+    public void saveMappings(ActionEvent actionEvent) {
+
+    }
+
+    public void onClose(ActionEvent actionEvent) {
+        System.exit(0);
+    }
+}
