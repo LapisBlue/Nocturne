1f011f46d2a8c6b60da04e7f2244ef21f97052a5
Max Roncace <me@caseif.net>
2016-02-13 13:17:34 -0500

    Add 'null', 'true', and 'false' as syntax keywords

--- src/main/java/blue/lapis/nocturne/util/JavaSyntaxHighlighter.java
+++ src/main/java/blue/lapis/nocturne/util/JavaSyntaxHighlighter.java
@@ -48,13 +48,14 @@ public final class JavaSyntaxHighlighter {
             "abstract", "assert", "boolean", "break", "byte",
             "case", "catch", "char", "class", "const",
             "continue", "default", "do", "double", "else",
-            "enum", "extends", "final", "finally", "float",
-            "for", "goto", "if", "implements", "import",
-            "instanceof", "int", "interface", "long", "native",
-            "new", "package", "private", "protected", "public",
-            "return", "short", "static", "strictfp", "super",
-            "switch", "synchronized", "this", "throw", "throws",
-            "transient", "try", "void", "volatile", "while"
+            "enum", "extends", "false", "final", "finally",
+            "float", "for", "goto", "if", "implements",
+            "import", "instanceof", "int", "interface", "long",
+            "native", "new", "null", "package", "private",
+            "protected", "public", "return", "short", "static",
+            "strictfp", "super", "switch", "synchronized", "this",
+            "throw", "throws", "transient", "true", "try",
+            "void", "volatile", "while"
     };
 
     private static final String KEYWORD_PATTERN = "\\b(" + String.join("|", KEYWORDS) + ")\\b";
