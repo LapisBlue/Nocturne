bef85f9b30517bd82918892d7c94aa049fc8c8ee
Jamie Mansfield <dev@jamierocks.uk>
2016-08-17 15:11:30 +0100

    Support for reading POMF mappings

--- src/main/java/blue/lapis/nocturne/mapping/io/reader/PomfReader.java
+++ src/main/java/blue/lapis/nocturne/mapping/io/reader/PomfReader.java
@@ -27,19 +27,46 @@ package blue.lapis.nocturne.mapping.io.reader;
 
 import blue.lapis.nocturne.mapping.MappingContext;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.stream.Collectors;
 
 /**
  * The mappings reader for the POMF format.
  */
-public class PomfReader extends MappingsReader {
+public class PomfReader extends EnigmaReader {
 
-    public PomfReader(Path mappingsPath) {
-        super(null); // TODO:
+    public PomfReader(Path mappingsPath) throws IOException {
+        super(readPomfMappings(mappingsPath));
     }
 
     @Override
     public MappingContext read(MappingContext mappings) {
-        return mappings;
+        return super.read(mappings);
+    }
+
+    private static BufferedReader readPomfMappings(Path mappingsPath) throws IOException {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        PrintWriter writer = new PrintWriter(out);
+
+        Files.walk(mappingsPath).filter(Files::isRegularFile).forEach(path -> {
+            try (BufferedReader reader = new BufferedReader(new FileReader(path.toFile()))) {
+                reader.lines().collect(Collectors.toList()).forEach(writer::println);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        ByteArrayInputStream input = new ByteArrayInputStream(out.toByteArray());
+        writer.close();
+        out.close();
+        return new BufferedReader(new InputStreamReader(input));
     }
 }
